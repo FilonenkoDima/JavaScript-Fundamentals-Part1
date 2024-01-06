79225951f8014b18e7bd7ebc9db1374454d21c2e
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-06 16:37:28 +0200

    values and variables

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let js = 'amazing';
-if (js === 'amazing')
-   alert('JavaScript is FUN!');
-
-
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let $new = "JM";
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
