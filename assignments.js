5d4eb42c07b2108f2b75ecd5c32d7f3e6191e944
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-06 17:07:50 +0200

    lecture: Data Types

--- assignments.js
+++ assignments.js
@@ -1,7 +1,10 @@
-let country = "Ukraine";
+//asigment to lecture - Values and Variables
+/*let country = "Ukraine";
 let continent = "Europe";
 let population = 40;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+*/
+
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let js = 'amazing';
+/*let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 console.log('Jonas');
@@ -10,6 +10,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+//lecture: Variable name conventions
 let $new = "JM";
 let PI = 3.1415;
 
@@ -17,3 +18,26 @@ let myFirstJob = 'Coder';
 let myCurrentJob = 'Teacher';
 
 console.log(myFirstJob);
+*/
+
+// lecture: Data types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Hello");
+
+javascriptIsFun = 23;
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
