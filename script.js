1f355b8c13c3766ed6ebd0a8b6d26057c9d3badd
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-07 22:33:41 +0200

    lecture: Equality Operators: == vs ===

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ console.log('23' / "2");
 let n = '1' + 1; // '11'
 n -= 1; // 10
 console.log(n);
-*/
+
 
 //Truthy and Falsy Values
 
@@ -208,3 +208,34 @@ if (height) {
 } else {
    console.log('Height is UNDEFIED');
 }
+*/
+
+//Equality Operators: == vs ===
+
+const age = 18;
+if (age === 18) console.log('You just became an adult :D (strict) num - num');
+if (age == 18) console.log('You just became an adult :D (loose) num - num');
+
+const ageString = '18';
+if (ageString === 18) console.log('You just became an adult :D (strict) str - num');
+if (ageString == 18) console.log('You just became an adult :D (loose) str - num');
+
+const favourite = Number(prompt("What's yoru favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite == 27) {
+   console.log('Cool! 27 is an amazing number!');
+}
+
+if (favourite === 27) {
+   console.log('Cool! 27 is an amazing number!');
+} else if (favourite === 7) {
+   console.log('7 is also a cool number');
+} else if (favourite === 9) {
+   console.log('9 is also a cool number');
+} else {
+   console.log('Number is not 23 or 7 or 9');
+}
+
+if (favourite !== 27) console.log('Why not 27?');
